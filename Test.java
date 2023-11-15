9801b196a0a75cc0fb15d0d759461d48d9bd5c4d
coopons <oneplusx5@gmail.com>
2023-11-15 12:42:04 -0700

    Changed strategy A to B

--- Test.java
+++ Test.java
@@ -5,11 +5,11 @@ public class Test {
 	public static void main(String[] args) {
 		Game g = new Game(1000);
 
-//		g.strategyA();
-//		g.printBoard();
-
-		g.strategyB();
+		g.strategyA();
 		g.printBoard();
+
+//		g.strategyB();
+//		g.printBoard();
 	
 	}
 }
