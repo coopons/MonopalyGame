42c572622dcc7c7a1f396761c581ccea2c9899a5
Cooper Pons <oneplusx5@gmail.com>
2023-11-15 12:48:30 -0700

    Changed strategy A to B

--- Test.java
+++ Test.java
@@ -1,10 +1,11 @@
 package MonopalyGame;
 
 public class Test {
+	// TODO Package name to "Monopoly Game"
 
 	public static void main(String[] args) {
 		Game g = new Game(1000);
-
+		// Change to whatever strategy is to be used
 		g.strategyA();
 		g.printBoard();
 
